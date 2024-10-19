a0a722722c3f73e0a5c30a7d82c8dcaa5f203ce6
BKDR01 <bahodirov.bobur@icloud.com>
2024-10-19 19:38:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let rondom = Math.floor(Math.random() * 100)
+let al = alert(`Randomdan kelgan son: ${rondom}`)
+let res = rondom * 2 
+let res2 = res / 2
+let res3 = res2 / 3
+let ob = alert(`Randomdan kelgan son ${rondom} edi \n biz uni 2ga Ko'paytirdik ${res} chiqardik \n va shu sonni 2 ga bo'lib ${res2} sonini chiqardik \n va yana shu sonni 3ga bo'lib ${res3} sonini chiqardik \n javob: ${res3}`)
